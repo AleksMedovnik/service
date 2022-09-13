0c9e4662f1f827965bb172ff66861df64d040922
Aleksandr <medovnikaleksander@yandex.ua>
2022-09-13 13:26:37 +0300

    added APIView

--- drfsite/service/views.py
+++ drfsite/service/views.py
@@ -1,8 +1,19 @@
-from rest_framework import generics
+from django.forms import model_to_dict
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import User
-from .serializers import UserSerializer
 
 
-class UserAPIView(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class UserAPIView(APIView):
+
+    def get(self, request):
+        lst = User.objects.all().values()
+        return Response({'posts': list(lst)})
+
+    def post(self, request):
+        post_new = User.objects.create(
+            title = request.data['title'],
+            content = request.data['content'],
+            cat_id = request. data['cat_id']
+        )
+        return Response({'post': model_to_dict(post_new)})
