ba2aa0bf4843aa563a4f4d57b0f85dc59f4c82e2
Aleksandr <medovnikaleksander@yandex.ua>
2022-09-13 22:35:33 +0300

    added sererializer

--- drfsite/service/serializers.py
+++ drfsite/service/serializers.py
@@ -1,8 +1,34 @@
 from rest_framework import serializers
-from .models import User
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+import io
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('title', 'cat_id')
+# class UserModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
+
+class UserSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
+
+
+# def encode():
+#     model = UserModel('Fedya', 'Oh my life...')
+#     model_sr = UserSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
+#
+#
+# def decode():
+#     stream = io.BytesIO(b'{"title":"Fedya","content":"Oh my life..."}')
+#     data = JSONParser().parse(stream)
+#     serializer = UserSerializer(data=data)
+#     serializer.is_valid()
+#     print(serializer.validated_data)
--- drfsite/service/views.py
+++ drfsite/service/views.py
@@ -2,18 +2,21 @@ from django.forms import model_to_dict
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import User
+from .serializers import UserSerializer
 
 
 class UserAPIView(APIView):
 
     def get(self, request):
-        lst = User.objects.all().values()
-        return Response({'posts': list(lst)})
+        u = User.objects.all()
+        return Response({'posts': UserSerializer(u, many=True).data})
 
     def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         post_new = User.objects.create(
-            title = request.data['title'],
-            content = request.data['content'],
-            cat_id = request. data['cat_id']
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
         )
-        return Response({'post': model_to_dict(post_new)})
+        return Response({'post': UserSerializer(post_new).data})
